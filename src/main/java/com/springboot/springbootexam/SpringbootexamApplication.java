729ffa854fb1ed908c2070bacfbc918f9a48dc6f
dev-swd <tladnjsen@gmail.com>
2021-03-30 23:24:35 +0900

    [2021-03-30:SWD] java11/springboot/maven/mybatis study. First commit.

---
+++ src/main/java/com/springboot/springbootexam/SpringbootexamApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootexamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/springbootexam/SpringbootexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
